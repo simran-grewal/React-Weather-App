2931c7f5e66e3f54f63c421f3c1f7eeb85d7f084
Harman Grewal <simrangrewal587@gmail.com>
2017-05-09 17:15:12 +0530

    Fixing LocalHost

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const port = process.env.PORT || 3000;
 
 
 app.use(function(req, res, next) {
-	if (req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else {
+	// req.headers is for Heroku it Does'not Exist Locally
+	if (req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 
