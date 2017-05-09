07d3438058eae66e20caf83c7789e23a77fb6161
Harman Grewal <simrangrewal587@gmail.com>
2017-05-09 18:41:49 +0530

    Add foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -5,11 +5,15 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
+
+
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <Route path = "about" component = {About}/>
-      <Route path  = "examples" component = {Examples}/> 
+      <Route path  = "examples" component = {Examples}/>
       <IndexRoute component = {Weather}/>
     </Route>
   </Router>,
